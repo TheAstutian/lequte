4f24ce6024367a4443bcaedb882e5c419a0d4465
TheAstutian <speakingatoms@gmail.com>
2023-06-02 21:40:31 +0100

    structure of site

---
+++ src/components/Heroarea/Heroarea.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+const Heroarea= ()=>{
+
+    return (
+      <div>
+          <div className="App">
+            <h1>Emmanuel</h1>
+            <p>This is my website</p>
+          </div>
+          
+          <div>
+            
+          </div>
+      </div>
+    )
+}
+
+export default Heroarea;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Navigation = ()=>{
+    return (
+        <div>
+            This is navigation
+        </div>
+    )
+}
+
+export default Navigation;
