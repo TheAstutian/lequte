0fbf3b61f280d2efde4ca91c22299bac3fed10cb
TheAstutian <speakingatoms@gmail.com>
2023-06-13 00:46:33 +0100

    structuring Projects.js

---
+++ src/components/Project/Project.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Project =()=>{
+
+    return (
+        <div>
+            name
+            description
+            image
+            tech used 
+            project link 
+            github link 
+        </div>
+    )
+}
+
+export default Project; 
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,20 +1,51 @@
 import React from "react";
 
 
-const Projects= ()=>{
-    const h2Style={
-      color: "white", 
-      fontSize: "2rem",
-      fontWeight: "lighter"
-    }
-    return (
+const data=[
+  { 
+    project_name: "Brain",
+    description: "description",
+    image:'',
+    tech_used: "Reactjs, Nodejs, Postgresql", 
+    project_link: "",
+    github_link:''
+
+  },
+  
+  {
+    project_name: "Brain2",
+    description: "description",
+    image:'img',
+    tech_used: "Reactjs, Nodejs, Postgresql", 
+    project_link: "",
+    github_link:''
+  }
+]
+
+
+class Projects extends React.Component {
+
+  
+  
+    render(){
+
+      const h2Style={
+        color: "white", 
+        fontSize: "2rem",
+        fontWeight: "lighter"
+      }
+
+      return (
       <div className="mt5 pt4 pl4 ml2"> 
         <h2 style={h2Style}>Projects</h2>
           <div>
-
+            {data.map((item)=>{
+           return   <p><a href="{item.project_link}">{item.project_name}</a></p>
+          } )}
         </div>
       </div>
     )
+}
 }
 
 export default Projects;
